f1e9da89a4775812987af5c4400d401fab6f3cd7
Isaac-baqe-botros <isaacbaqe.botros@gmail.com>
2022-05-16 16:47:52 +0200

    Add project files.

---
+++ Hotel_Booking_System/Models/Room_Usage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Booking_System.Models
+{
+    public class Room_Usage
+    {
+        public int Room_id { get; set; }
+    }
+}
---
+++ Hotel_Booking_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotel_Booking_System.Startup))]
+namespace Hotel_Booking_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
